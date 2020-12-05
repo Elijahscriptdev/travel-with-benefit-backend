b448720dff5b057f95fac5b8008a8cdb917b12aa
Elijahscriptdev <elijahobominuru@gmail.com>
2020-12-05 12:23:42 +0100

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,19 +14,19 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   
-    # allow do
-    #   origins "http://localhost:3001"
-    #   resource "*", headers: :any, methods: [
-    #       :get, :post, :put, :patch, :delete, :options, :head
-    #   ],
-    #   credentials: true
-    # end
+    allow do
+      origins "http://localhost:3000"
+      resource "*", headers: :any, methods: [
+          :get, :post, :put, :patch, :delete, :options, :head
+      ],
+      credentials: true
+    end
 
-    # allow do
-    #   origins "https://serene-jepsen-7b0096.netlify.app"
-    #   resource "*", headers: :any, methods: [
-    #       :get, :post, :put, :patch, :delete, :options, :head
-    #   ],
-    #   credentials: true
-    # end
+    allow do
+      origins "https://serene-jepsen-7b0096.netlify.app"
+      resource "*", headers: :any, methods: [
+          :get, :post, :put, :patch, :delete, :options, :head
+      ],
+      credentials: true
+    end
   end
